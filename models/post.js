8779a05c5d758849578bdade98a61bc7bfedd58b
Ian Rones <ronesp.85@gmail.com>
2021-04-14 10:02:25 -0700

    [add] code to server.js

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema  = new Schema({
+  title: {
+    type: String,
+    required:true,
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true,
+  }
+},
+  { timestamps: {createdAt: 'created_at'} }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+
 const port = 3000
-
-app.use(express.static('public'));
-
-app.get('/', (req,res) => {
-  res.render('layouts/main');
-})
+const app = express();
+require('./data/reddit-db')
+require('./controllers/posts')(app)
 
 const exphbs = require('express-handlebars')
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}))
+
+// Add after body parser initialization
+app.use(expressValidator());
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
+
+app.get('/', (req,res) => {
+  res.render('home');
+})
+
+app.get('/posts/new', (req,res) => {
+  console.log('We just posted a new post')
+  res.render('posts-new');
+})
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
